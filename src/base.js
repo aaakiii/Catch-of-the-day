7eb74b05e6acc49a7bbcd8ca54187979ba5ca33e
aaakiii <a.okada3@gmail.com>
2018-03-05 13:41:03 -0500

    add project

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCthRiMUIYXGNa-WwZGc-adXHHMYpkHcBA",
+    authDomain: "catch-of-the-day-881c6.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-881c6.firebaseio.com"
+
+});
+
+export default base;
